de51c74fc4fef1da807be64fcb4fd9a561e2b144
Slash <so.slashua@gmail.com>
2025-11-02 14:24:45 +0200

    Add logger && 3 label times

---
+++ src/front-end/components/ui/select/InputSelect/processingOptions.ts
@@ -0,0 +1,23 @@
+import {InputSelectOptionsBaseType, InputSelectOptionsProps} from "@/front-end/types/props/InputSelect.props"
+
+export function processingOptions(
+    options: InputSelectOptionsProps[]
+): InputSelectOptionsBaseType[] {
+    const first = options[0]
+
+
+    if (typeof first === 'number') {
+        return (options as number[]).map(option => ({
+            value: String(option),
+            label: String(option),
+        }))
+    }else if (typeof first === 'object' && first !== null) {
+        return (options as { value: string; label: string }[])
+    }else{
+        return (options as string[]).map(option => ({
+            value: option,
+            label: option,
+        }))
+    }
+
+}
